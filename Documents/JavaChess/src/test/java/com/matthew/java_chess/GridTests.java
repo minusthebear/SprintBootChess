160cfaf8f85da95378f5ab653a99b4150060c3fb
matthewhamann <matt.hamann1982@gmail.com>
2018-12-16 12:39:45 -0800

    Removed Jupiter

--- Documents/JavaChess/src/test/java/com/matthew/java_chess/GridTests.java
+++ Documents/JavaChess/src/test/java/com/matthew/java_chess/GridTests.java
@@ -5,7 +5,6 @@ import com.matthew.java_chess.entities.Pawn;
 import com.matthew.java_chess.entities.Piece;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
--- Documents/JavaChess/src/test/java/com/matthew/java_chess/QueenTests.java
+++ Documents/JavaChess/src/test/java/com/matthew/java_chess/QueenTests.java
@@ -21,6 +21,6 @@ public class QueenTests {
 
     @Test
     public void moveQueen() {
-        
+
     }
 }
